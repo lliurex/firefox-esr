3eb5ce78d94911ddd77e54a3a8f4a4f58c3d57ee
juanma1980 <juanma1980@gmail.com>
2019-03-26 09:31:40 +0100

    Update to 52.9

---
+++ firefox-common/distribution/extensions/langpack-es-ES@firefox.mozilla.org/browser/defaults/preferences/firefox-l10n.js
@@ -0,0 +1,5 @@
+//@line 4 "/home/juanma/trabajo/ramfs/locale/l10n/es-ES/browser/firefox-l10n.js"
+
+//@line 6 "/home/juanma/trabajo/ramfs/locale/l10n/es-ES/browser/firefox-l10n.js"
+
+pref("general.useragent.locale", "es-ES");
